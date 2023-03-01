c7f777e10f984f83988f9653a6624095e3b901a6
Cheyenne Edgar <cheyennerivers03@gmail.com>
2023-03-01 10:19:15 -0800

    Created a view for the Student Roster

--- CPW219-CRUD-Troubleshooting/Controllers/HomeController.cs
+++ CPW219-CRUD-Troubleshooting/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace CPW219_CRUD_Troubleshooting.Controllers
             return View();
         }
 
+        public IActionResult Student()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
