78ce92ba564021f9914b7fb3ee74a262b02322d6
Cheyenne Edgar <cheyennerivers03@gmail.com>
2023-03-01 10:18:34 -0800

    Created folder for StudentContext

---
+++ CPW219-CRUD-Troubleshooting/Data/SchoolContext.cs
@@ -0,0 +1,15 @@
+﻿using CPW219_CRUD_Troubleshooting.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CPW219_CRUD_Troubleshooting.Data
+{
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext(DbContextOptions<SchoolContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
